04a9eab848951c49bebd7dc01211d07e83376c5e
megha77777 <141833704+megha77777@users.noreply.github.com>
2023-12-26 23:25:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
+let submitBtn = document.getElementById("submit")
+let firstName = document.getElementById("firstname")
+let lastName = document.getElementById("lastname")
+	function getFormvalue() {
     //Write your code here
-
+		
+	submitBtn.addEventListener("click", ()=>{
+		alert(firstName.value+" "+lastName.value);
+	})
 }
+getFormvalue()
